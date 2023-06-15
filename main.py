440d36a497514a6f962ca372ee350fcff01a7af4
nakata5321 <makar5321@gmail.com>
2023-06-15 17:59:52 +0300

    fix problems

--- main.py
+++ main.py
@@ -12,25 +12,25 @@ from config import (
     DIGITAL_TWIN_TOPIC,
     GOOD_POSITION,
     BAD_POSITION,
-    MOVE_SPEED
+    MOVE_SPEED,
 )
 
 
-class MyCobot:
+class Cobot280:
     """
     simple class to control my cobot 280jn
     """
 
     def __init__(self) -> None:
-        self.account = Account()
+        self.account = Account(seed=SEED)
         self.mc = MyCobot(SERIAL_PORT, BAUD_RATE)
-        self.dt = DigitalTwin(SEED)
+        self.dt = DigitalTwin(self.account)
         self.sub_address = self.dt.get_source(DIGITAL_TWIN_NUMBER, DIGITAL_TWIN_TOPIC)
         self.subscriber = Subscriber(
             self.account,
             SubEvent.NewRecord,
             addr=self.sub_address,
-            subscription_handler=self.callback
+            subscription_handler=self.callback,
         )
         # At start go to "home position"
         self.go_home_position()
@@ -41,7 +41,7 @@ class MyCobot:
         """
         self.mc.send_angles([0, 0, 0, 0, 0, 0], MOVE_SPEED)
 
-    def callback(self, data_raw: list[tp.Union[str, dict[str, str]]]) -> None:
+    def callback(self, data_raw: tp.List[tp.Union[str, tp.Dict[str, str]]]) -> None:
         """
         callback function for robonomics subscribers
         :param data_raw: data from datalog
@@ -69,11 +69,11 @@ class MyCobot:
                 print("in loop")
                 time.sleep(10)
         except KeyboardInterrupt:
-            print('interrupted!')
+            print("interrupted!")
 
         self.subscriber.cancel()
 
 
 if __name__ == "__main__":
-    my_cobot = MyCobot()
+    my_cobot = Cobot280()
     my_cobot.run()
