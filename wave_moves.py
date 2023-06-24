7890d0399f4133e6087a4f4e76d9d2d02ac2e2cb
Vadim Manaenko <vadim.razorq@gmail.com>
2023-06-24 16:45:05 +0300

    fix command

--- wave_moves.py
+++ wave_moves.py
@@ -16,13 +16,13 @@ print(angles)
 # Intelligently plan the route, let the head reach the coordinates of [57.0, -107.4, 316.3] in a linear manner,
 # and maintain the attitude of [-93.81, -12.71, -163.49], the speed is 80mm/s
 for i in range(0, 2):
-    mc.send_angles([0, 0, 0, 0, 0, 0], 10, 1)
+    mc.send_angles([0, 0, 0, 0, 0, 0], 10)
     time.sleep(1)
 
 
-    mc.send_angles([-0.08, -74.26, 0.08, -0.87, 0.35, -0.61], 10, 1)
+    mc.send_angles([-0.08, -74.26, 0.08, -0.87, 0.35, -0.61], 10)
     time.sleep(1.5)
 
-    mc.send_angles([0, 0, 0, 0, 0, 0], 10, 1)
+    mc.send_angles([0, 0, 0, 0, 0, 0], 10)
     time.sleep(1)
 
